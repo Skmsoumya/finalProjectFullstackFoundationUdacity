28874d009a4a00a4aedd20ae8caa63c3029c5974
Skmsoumya <soumyakishormishra@gmail.com>
2017-04-16 23:59:02 +0530

    completed adding google o auth code

--- project.py
+++ project.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request, jsonify, flash
+from flask import session as login_session
+import string, random
 
-from dummyData import dummyData
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 
@@ -14,6 +21,8 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+CLIENT_ID = json.loads(open('client_secret.json', 'r').read())["web"]["client_id"]
 
 @app.route("/")
 @app.route("/restaurants")
@@ -140,6 +149,72 @@ def apiRestaurantMenuItem(restaurant_id):
 def apiSingleMenuItem(restaurant_id, menu_id):
 	menuItem = session.query(MenuItem).filter_by(id = menu_id).one()
 	return jsonify(restaurant=menuItem.serialize)
+
+@app.route("/login")
+def showLogin():
+	state = "".join([random.choice(string.ascii_uppercase + string.digits) for x in xrange(32)])
+	login_session["state"] = state
+	return render_template("login.html", state = login_session["state"])
+
+@app.route("/gconnect", methods=["POST"])
+def gconnect():
+	if(request.args.get("state") != login_session["state"]):
+		response = make_response(json.dumps("Invalid State Params!"), 401)
+		response.headers["Content-Type"] = "application/json"
+		return response
+	code = request.data
+	try:
+		#Upgrade authorization code to a credentials object
+		oauth_flow = flow_from_clientsecrets("client_secret.json", scope="")
+		oauth_flow.redirect_url = "postmessage"
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps("Failed to convert authorization code to credentials!"), 401)
+		response.headers["Content-Type"] = "application/json"
+		return response
+
+	access_token = credentials.access_token
+	url = ("https://googleapis.com/oauth2/v1/tokeninfo?access_token=%s" % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, "GET")[1])
+
+	#If error in access token info abort
+	if result["error"] is not None:
+		response = make_response(json.dumps(result.get("error")), 500)
+		response.headers["Content-Type"] = "application/json"
+		return response
+	#Verify that the access token is for correct user
+	gplus_id = credentials.id_token["sub"]
+	if result["issued_to"] == gplus_id:
+		response = make_response(json.dumps("Tokens Client Id doesn't matches apps"), 401)
+		print "Tokens Client Id doesn't matches apps"
+		response.headers["Content-Type"] = "application/json"
+		return response
+
+	# Check to see if user already logged in
+	stored_credentials = login_session.get("credentials")
+	stored_gplus_id = login_session.get("gplus_id")
+	if(stored_credentials is not None and gplus_id == stored_gplus_id):
+		response = make_response(json.dump("Current user is already logged in!"), 200)
+		response.headers["Content-Type"] = "application/json"
+
+	# store the user credentials and user id in the current session
+	login_session["credentials"] = credentials
+	login_session["gplus_id"] = gplus_id
+
+
+	#Get user info
+	user_info_url = "http://www.googleapis.com/oauth2/v1/userinfo"
+	params = {"access_token": credentials.access_token, "alt": "json"}
+	answer = requests.get(user_info_url, params = params)
+	data = json.loads(answer.text)
+
+	login_session["user_name"] = data["name"]
+	login_session["picture"] = data["picture"]
+	login_session["email"] = data["email"]
+	flash("You are now logged in as %s" % login_session["user_name"])
+	return redirect(url_for("homepageRoute"))
+
 
 @app.after_request
 def afterRequest(response):
